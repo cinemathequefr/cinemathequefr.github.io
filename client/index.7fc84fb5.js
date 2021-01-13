d40e88d6df9f7777addc7169be63122ca0128848
nltesown <nltesown@users.noreply.github.com>
2021-01-13 18:54:40 +0000

    deploy: 4242f498f2287f4b42be7665e47cbf2c39027d2c

---
+++ client/index.7fc84fb5.js
@@ -0,0 +1,1 @@
+import{S as t,i as s,s as e,q as a,d as i,n}from"./client.6f41ae31.js";function l(t){return{c(){this.h()},l(t){a('[data-svelte="svelte-1kiwl31"]',document.head).forEach(i),this.h()},h(){document.title="[CFv35] La Cinémathèque française"},m:n,p:n,i:n,o:n,d:n}}export default class extends t{constructor(t){super(),s(this,t,null,l,e,{})}}
